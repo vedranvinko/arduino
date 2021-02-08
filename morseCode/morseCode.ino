8aafa6fadeb462b3abe93e696b87d86871a535c0
vedran <vinkovedran@gmail.com>
2021-02-08 08:56:41 +0100

    Add morse code

---
+++ morseCode/morseCode.ino
@@ -0,0 +1,141 @@
+#define PIN_OUT        13
+#define UNIT_LENGTH    250
+
+static const struct {const char letter, *code;} MorseMap[] =
+{
+  { 'A', ".-" },
+  { 'B', "-..." },
+  { 'C', "-.-." },
+  { 'D', "-.." },
+  { 'E', "." },
+  { 'F', "..-." },
+  { 'G', "--." },
+  { 'H', "...." },
+  { 'I', ".." },
+  { 'J', ".---" },
+  { 'K', ".-.-" },
+  { 'L', ".-.." },
+  { 'M', "--" },
+  { 'N', "-." },
+  { 'O', "---" },
+  { 'P', ".--." },
+  { 'Q', "--.-" },
+  { 'R', ".-." },
+  { 'S', "..." },
+  { 'T', "-" },
+  { 'U', "..-" },
+  { 'V', "...-" },
+  { 'W', ".--" },
+  { 'X', "-..-" },
+  { 'Y', "-.--" },
+  { 'Z', "--.." },
+  { ' ', "     " },
+    
+  { '1', ".----" },
+  { '2', "..---" },
+  { '3', "...--" },
+  { '4', "....-" },
+  { '5', "....." },
+  { '6', "-...." },
+  { '7', "--..." },
+  { '8', "---.." },
+  { '9', "----." },
+  { '0', "-----" },
+    
+  { '.', "·–·–·–" },
+  { ',', "--..--" },
+  { '?', "..--.." },
+  { '!', "-.-.--" },
+  { ':', "---..." },
+  { ';', "-.-.-." },
+  { '(', "-.--." },
+  { ')', "-.--.-" },
+  { '"', ".-..-." },
+  { '@', ".--.-." },
+  { '&', ".-..." },
+};
+
+void setup()
+{
+  pinMode( PIN_OUT, OUTPUT );
+  digitalWrite( PIN_OUT, LOW );
+}
+
+void loop()
+{
+  String morseWord = encode( "SOS " );
+    
+  for(int i=0; i<=morseWord.length(); i++)
+  {
+    switch( morseWord[i] )
+    {
+      case '.':
+        digitalWrite( PIN_OUT, HIGH );
+        delay( UNIT_LENGTH );
+        digitalWrite( PIN_OUT, LOW );
+        delay( UNIT_LENGTH );
+          
+        break;
+
+      case '-':
+        digitalWrite( PIN_OUT, HIGH );
+        delay( UNIT_LENGTH*3 );
+        digitalWrite( PIN_OUT, LOW );
+        delay( UNIT_LENGTH );
+          
+        break;
+
+      case ' ':
+        delay( UNIT_LENGTH );
+    }
+  }
+}
+
+String encode(const char *string)
+{
+  size_t i, j;
+  String morseWord = "";
+  
+  for( i = 0; string[i]; ++i )
+  {
+    for( j = 0; j < sizeof MorseMap / sizeof *MorseMap; ++j )
+    {
+      if( toupper(string[i]) == MorseMap[j].letter )
+      {
+        morseWord += MorseMap[j].code;
+        break;
+      }
+    }
+    morseWord += " ";
+  }
+
+  return morseWord;  
+}
+
+String decode(String morse)
+{
+  String msg = "";
+  
+  int lastPos = 0;
+  int pos = morse.indexOf(' ');
+  while( lastPos <= morse.lastIndexOf(' ') )
+  {    
+    for( int i = 0; i < sizeof MorseMap / sizeof *MorseMap; ++i )
+    {
+      if( morse.substring(lastPos, pos) == MorseMap[i].code )
+      {
+        msg += MorseMap[i].letter;
+      }
+    }
+
+    lastPos = pos+1;
+    pos = morse.indexOf(' ', lastPos);
+    
+    while( morse[lastPos] == ' ' && morse[pos+1] == ' ' )
+    {
+      pos ++;
+    }
+  }
+
+  return msg;
+}
